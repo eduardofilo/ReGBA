adf3d870d060c9d9b9cef836f2a2c93efececf33
jdgleaver <james@leaver.myzen.co.uk>
2020-09-21 15:22:57 +0100

    Fix colour correction gamma adjustment level

--- source/opendingux/tools/generate_cc_lut.c
+++ source/opendingux/tools/generate_cc_lut.c
@@ -29,47 +29,47 @@ static uint16_t c_lut[CC_LUT_SIZE] = {0};
 
 void init_lut(void)
 {
-	size_t color;
-	static const float display_gamma_inv = 1.0f / CC_TARGET_GAMMA;
-	static const float rgb_max_inv       = 1.0f / CC_RGB_MAX;
-	static const float adjusted_gamma    = CC_TARGET_GAMMA * CC_GAMMA_ADJ;
+   size_t color;
+   float display_gamma_inv = 1.0f / CC_TARGET_GAMMA;
+   float rgb_max_inv       = 1.0f / CC_RGB_MAX;
+   float adjusted_gamma    = CC_TARGET_GAMMA + CC_GAMMA_ADJ;
 
-	/* Populate colour correction look-up table */
-	for (color = 0; color < CC_LUT_SIZE; color++)
-	{
-		unsigned r_final = 0;
-		unsigned g_final = 0;
-		unsigned b_final = 0;
-		/* Extract values from BGR555 input */
-		const unsigned r = color       & 0x1F;
-		const unsigned g = color >>  5 & 0x1F;
-		const unsigned b = color >> 10 & 0x1F;
-		/* Perform gamma expansion */
-		float r_float = pow((float)r * rgb_max_inv, adjusted_gamma);
-		float g_float = pow((float)g * rgb_max_inv, adjusted_gamma);
-		float b_float = pow((float)b * rgb_max_inv, adjusted_gamma);
-		/* Perform colour mangling */
-		float r_correct = CC_LUM * ((CC_R  * r_float) + (CC_GR * g_float) + (CC_BR * b_float));
-		float g_correct = CC_LUM * ((CC_RG * r_float) + (CC_G  * g_float) + (CC_BG * b_float));
-		float b_correct = CC_LUM * ((CC_RB * r_float) + (CC_GB * g_float) + (CC_B  * b_float));
-		/* Range check... */
-		r_correct = r_correct > 0.0f ? r_correct : 0.0f;
-		g_correct = g_correct > 0.0f ? g_correct : 0.0f;
-		b_correct = b_correct > 0.0f ? b_correct : 0.0f;
-		/* Perform gamma compression */
-		r_correct = pow(r_correct, display_gamma_inv);
-		g_correct = pow(g_correct, display_gamma_inv);
-		b_correct = pow(b_correct, display_gamma_inv);
-		/* Range check... */
-		r_correct = r_correct > 1.0f ? 1.0f : r_correct;
-		g_correct = g_correct > 1.0f ? 1.0f : g_correct;
-		b_correct = b_correct > 1.0f ? 1.0f : b_correct;
-		/* Convert back to BGR555 */
-		r_final = (unsigned)((r_correct * CC_RGB_MAX) + 0.5f) & 0x1F;
-		g_final = (unsigned)((g_correct * CC_RGB_MAX) + 0.5f) & 0x1F;
-		b_final = (unsigned)((b_correct * CC_RGB_MAX) + 0.5f) & 0x1F;
-		c_lut[color] = 1 << 15 | b_final << 10 | g_final << 5 | r_final;
-	}
+   /* Populate colour correction look-up table */
+   for (color = 0; color < CC_LUT_SIZE; color++)
+   {
+      unsigned r_final = 0;
+      unsigned g_final = 0;
+      unsigned b_final = 0;
+      /* Extract values from BGR555 input */
+      const unsigned r = color       & 0x1F;
+      const unsigned g = color >>  5 & 0x1F;
+      const unsigned b = color >> 10 & 0x1F;
+      /* Perform gamma expansion */
+      float r_float = pow((float)r * rgb_max_inv, adjusted_gamma);
+      float g_float = pow((float)g * rgb_max_inv, adjusted_gamma);
+      float b_float = pow((float)b * rgb_max_inv, adjusted_gamma);
+      /* Perform colour mangling */
+      float r_correct = CC_LUM * ((CC_R  * r_float) + (CC_GR * g_float) + (CC_BR * b_float));
+      float g_correct = CC_LUM * ((CC_RG * r_float) + (CC_G  * g_float) + (CC_BG * b_float));
+      float b_correct = CC_LUM * ((CC_RB * r_float) + (CC_GB * g_float) + (CC_B  * b_float));
+      /* Range check... */
+      r_correct = r_correct > 0.0f ? r_correct : 0.0f;
+      g_correct = g_correct > 0.0f ? g_correct : 0.0f;
+      b_correct = b_correct > 0.0f ? b_correct : 0.0f;
+      /* Perform gamma compression */
+      r_correct = pow(r_correct, display_gamma_inv);
+      g_correct = pow(g_correct, display_gamma_inv);
+      b_correct = pow(b_correct, display_gamma_inv);
+      /* Range check... */
+      r_correct = r_correct > 1.0f ? 1.0f : r_correct;
+      g_correct = g_correct > 1.0f ? 1.0f : g_correct;
+      b_correct = b_correct > 1.0f ? 1.0f : b_correct;
+      /* Convert back to BGR555 */
+      r_final = (unsigned)((r_correct * CC_RGB_MAX) + 0.5f) & 0x1F;
+      g_final = (unsigned)((g_correct * CC_RGB_MAX) + 0.5f) & 0x1F;
+      b_final = (unsigned)((b_correct * CC_RGB_MAX) + 0.5f) & 0x1F;
+      c_lut[color] = 1 << 15 | b_final << 10 | g_final << 5 | r_final;
+   }
 }
 
 int main ()
